81d1142a5c10b7efad8b6367d620d75e984c6207
abba <abba@workdeb>
2014-11-25 13:19:50 +0600

    fixed

--- Asset/JuiAsset.php
+++ Asset/JuiAsset.php
@@ -12,9 +12,9 @@ use yii\web\AssetBundle;
  */
 class JuiAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/jquery-ui';
+    public $sourcePath = '@vendor/xtlan/design/web';
     public $js = [
-        'jquery-ui.js',
+        'js/libs/jquery-ui-1.10.3.custom.js'
     ];
 
     public $depends = [
