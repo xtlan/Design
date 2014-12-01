e9bef2ee040ac657589d04ac56be183f1d35b6cf
abba <abba@workdeb>
2014-12-01 15:40:14 +0600

    JqueryDragSortAsset

---
+++ Asset/JqueryDragSortAsset.php
@@ -0,0 +1,27 @@
+<?php
+namespace Xtlan\Design\Asset;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+/**
+ * JqueryDragSortAsset
+ *
+ * @version 1.0.0
+ * @copyright Copyright 2011 by Kirya <cloudkserg11@gmail.com>
+ * @author Kirya <cloudkserg11@gmail.com>
+ */
+class JqueryDragSortAsset extends AssetBundle
+{
+
+    public $sourcePath = '@vendor/xtlan/design/web';
+    public $js = [
+        'js/libs/jquery.dragsort-0.5.2.min.js'
+    ];
+    public $depends = [
+        //'yii\web\JqueryAsset'
+    ];
+    
+
+}
+
+
